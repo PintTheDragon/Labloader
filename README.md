c97abf0edf51e7047538a5f83a44997d26a137cc
VirtualBrightPlayz <lego.trains.b@gmail.com>
2023-05-08 19:15:57 -0700

    Project setup script

--- README.md
+++ README.md
@@ -10,6 +10,14 @@ Labloader is an experimental plugin loader and framework for SCP: Labrat.
 
 ## Building from source
 
+### Automatic setup
+
+- Install .NET 5.0 SDK
+- Run `setup.ps1`
+- Open the project with IDE of choice
+
+### Manual setup
+
 - Install https://github.com/iRebbok/APublicizer via releases
 - Copy `Managed` folder from SCP: Labrat's Dedicated Server tool (Steam AppId is `2261800`) here, rename it to `LABRAT_DEPS`
 - Publicize `Assembly-CSharp.dll` and `RiptideNetworking.dll` files in the `LABRAT_DEPS` folder
---
+++ setup.ps1
@@ -0,0 +1,43 @@
+$extra_install_flags = ""
+# $extra_install_flags = "-beta unstable-beta"
+
+$work_dir = "./workspace/"
+$game_dir = $work_dir + "serverfiles/"
+$apublicizer_dir = $work_dir + "apublicizer/"
+$apublicizer_zip = $work_dir + "apublicizer.zip"
+$steamcmd_dir = $work_dir + "steamcmd/"
+$steamcmd_zip = $work_dir + "steamcmd.zip"
+$steamcmd = $steamcmd_dir + "steamcmd.exe"
+
+if (!(Test-Path -Path $work_dir -PathType Any)) {
+    New-Item -Path $work_dir -ItemType Directory
+}
+if (!(Test-Path -Path $steamcmd_dir -PathType Any)) {
+    New-Item -Path $steamcmd_dir -ItemType Directory
+}
+Write-Progress -Activity "Installing SteamCMD"
+if (!(Test-Path -Path $steamcmd_zip -PathType Any)) {
+    Invoke-WebRequest "https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip" -OutFile $steamcmd_zip
+    Expand-Archive -Path $steamcmd_zip -DestinationPath $steamcmd_dir
+}
+
+Write-Progress -Activity "Installing SCP: Labrat Dedicated Server tool"
+& $steamcmd ("+force_install_dir ../serverfiles/") "+login anonymous" ("+app_update 2261800 " + $extra_install_flags + " validate") "+quit"
+if (!(Test-Path -Path "./LABRAT_DEPS/" -PathType Any)) {
+    New-Item -Path "./LABRAT_DEPS/" -ItemType Directory
+}
+Copy-Item -Path ($game_dir + "SCP Labrat_Data/Managed/*") -Destination "./LABRAT_DEPS/" -Recurse
+
+Write-Progress -Activity "Installing APublicizer-1.0.3"
+if (!(Test-Path -Path $apublicizer_dir -PathType Any)) {
+    New-Item -Path $apublicizer_dir -ItemType Directory
+}
+if (!(Test-Path -Path $apublicizer_zip -PathType Any)) {
+    Invoke-WebRequest "https://github.com/iRebbok/APublicizer/archive/refs/tags/1.0.3.zip" -OutFile $apublicizer_zip
+    Expand-Archive -Path $apublicizer_zip -DestinationPath $apublicizer_dir
+}
+
+dotnet run -c Release --project "./workspace/apublicizer/APublicizer-1.0.3/APublicizer.csproj" "./LABRAT_DEPS/Assembly-CSharp.dll"
+dotnet run -c Release --project "./workspace/apublicizer/APublicizer-1.0.3/APublicizer.csproj" "./LABRAT_DEPS/RiptideNetworking.dll"
+
+Write-Output "Project is setup and ready!"
